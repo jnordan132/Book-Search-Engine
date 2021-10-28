6edd53fd94e629cfc6be1cbc21b93be8f416f060
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-10-28 15:25:59 -0400

    reseting node modules

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = 'mysecretsishere';
 const expiration = '2h';
 
 module.exports = {
